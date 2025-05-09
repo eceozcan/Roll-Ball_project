4ac4ff435923f11ae1883eee995da1f3c7fb4076
ece <ecemerve8@yahoo.com>
2025-05-09 14:20:10 +0300

    update bugs

--- Roll Ball/Assets/Scripts/CameraController.cs
+++ Roll Ball/Assets/Scripts/CameraController.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    private void FixedUpdate()
+    private Vector3 offset;
+
+    private void Start()
     {
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
         transform.LookAt(target);
     }
 }
